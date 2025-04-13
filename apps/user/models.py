4122c896c83f6ec2b52e608abea92c2f467464f9
Kevin Sean Kho <kevinseankho@yahoo.com>
2025-04-13 09:50:45 +0800

    Feat(user): add admin user seeder and update migrations (#5)
    - Removed redundant id_user field and finalized AbstractUser integration
    - Updated migration files to reflect changes to User model
    - Created management command 'seed_admin_user' and 'seed_usertypes' to create default data
    
    Co-authored-by: Kevin Sean Kho <kevin.kho@butterfly.com.au>

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.models import AbstractUser
 
 class UserType(models.Model):
     id = models.AutoField(primary_key=True)
@@ -7,14 +9,15 @@ class UserType(models.Model):
     def __str__(self):
         return self.name
 
-class User(models.Model):
-    id_user = models.AutoField(primary_key=True)
+class User(AbstractUser):
+    username = None
     user_type = models.ForeignKey(UserType, on_delete=models.PROTECT)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
-    is_active = models.BooleanField(default=True)
+    email = models.EmailField(unique=True)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'user_type']
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
