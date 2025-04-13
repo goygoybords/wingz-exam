c49a628e3b5f9f7e3774e4a2db2cb71d15ea286c
Kevin Sean Kho <kevin.kho@butterfly.com.au>
2025-04-13 09:07:16 +0800

    Feat(models): Create Ride, RideEvent, User, and UserType models.
    - Defined models based on the provided schema for Ride, User, and RideEvent.
    - Introduced a separate UserType model for role management.
    - Registered all models in the Django admin for development convenience.

--- apps/ride/admin.py
+++ apps/ride/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Ride, RideEvent
 
-# Register your models here.
+# Registering models for Django admin interface.
+# Best practice during development to easily view, edit, and verify model data.
+
+admin.site.register(Ride)
+admin.site.register(RideEvent)
