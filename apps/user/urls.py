f3d4cee18a7154346a66ffac9176a553993b4e67
Kevin Sean Kho <kevin.kho@butterfly.com.au>
2025-04-13 11:10:21 +0800

    Feat(user): implement User CRUD using DRF ModelViewSet with admin-only access
    - Added UserViewSet using Django REST Framework's ModelViewSet
    - Created UserSerializer supporting password, user_type_id, is_superuser, is_active, etc.
    - Restricted all user operations using a custom IsAdminRole permission
    - Registered endpoints via DefaultRouter under /api/user/users/
    - Verified functionality for Create, Read, Update, and Delete via Postman

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView
 from apps.user import views
 
+router = DefaultRouter()
+router.register(r'users', views.UserViewSet, basename='user')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('login/', views.LoginAPIView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,5 +1,14 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 from apps.user.serializers import CustomTokenObtainPairSerializer
+from rest_framework import viewsets
+from apps.user.models import User
+from apps.user.serializers import UserSerializer
+from apps.user.permissions import IsAdminRole
 
 class LoginAPIView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAdminRole]
