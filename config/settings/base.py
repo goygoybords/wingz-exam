b262a12297b7db62c5f14b708222eee3c7ed64f0
Kevin Sean Kho <kevinseankho@yahoo.com>
2025-04-14 17:06:25 +0800

    Feat(api): Implement pagination for Ride list API (#17)
    - Integrated page-based pagination using DRF's PageNumberPagination
    - Made PAGE_SIZE configurable via environment variable
    - Confirmed pagination functionality via Postman
    - Enhances performance and scalability of Ride listings
    
    Co-authored-by: Kevin Sean Kho <kevin.kho@butterfly.com.au>

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = []
 AUTH_USER_MODEL = 'user.User'
 BASE_URL = env('BASE_URL')
 DEBUG = env('DEBUG')
-
+PAGE_SIZE = env.int("PAGE_SIZE", default=10)
 
 # Application definition
 
@@ -133,6 +133,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': PAGE_SIZE,
 }
 
 SIMPLE_JWT = {
