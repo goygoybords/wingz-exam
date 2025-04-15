0272f3a8feaaa87dcdc7e2f20ee89c3c229d0796
Kevin Sean Kho <kevin.kho@butterfly.com.au>
2025-04-15 18:18:00 +0800

    feat: Set Django Developer Test as default home page
    The home page now displays the Python/Django Developer Test content,
    developed by Kevin Kho.

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,19 @@
 """
     URL configuration for wingz-exam project.
 """
-
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-
-
+from config.views import home
 
 urlpatterns = [
+    path('', home),  # 👈 set root route to home
     path('api/user/', include('apps.user.urls')),
     path('api/ride/', include('apps.ride.urls')),
 ]
 
 if settings.DEBUG:
+    from django.contrib import admin
     urlpatterns.append(path('admin/', admin.site.urls))
+
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
